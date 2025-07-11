12b953032ced57bcab589a0558619bd5e3128e0f
Chirag Dhawan <104852727+lifestorymeta@users.noreply.github.com>
2025-07-11 18:45:15 +0530

    Uploaded game code and README

---
+++ snake_water_gun.py.py
@@ -0,0 +1,28 @@
+import random
+
+def game_results(user, comp):
+    if(user==comp):
+        return "Draw!"
+    elif (user=="snake" and comp=="water") or \
+         (user=="gun" and comp=="snake") or \
+         (user== "water" and comp == "gun"):
+         return "You Win!"
+    else:
+        return "You Loose!"
+    
+choices = ["snake", "water", "gun"]
+comp_choice = random.choice(choices).lower()
+
+print("Welcome to Snake 🐍 Water 💧 Gun 🔫 Game!")
+print(f"Choose one: {choices}")
+
+user_choice = input("Enter your choice: ").lower()
+
+if(user_choice not in choices):
+    print("Invalid input. Please choose from Snake, Water, Gun.")
+
+else:
+    print(f"You chose: {user_choice}")
+    print(f"Computer chose: {comp_choice}")
+    print(game_results(user_choice, comp_choice))
+
